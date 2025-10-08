ba6be006a0bd525072add8b154a97622e40b345b
Guilherme Kilpp <guikilpp@gmail.com>
2025-10-08 12:08:20 -0300

    Added log to the routes

--- bitonic-scala3/src/main/scala/Main.scala
+++ bitonic-scala3/src/main/scala/Main.scala
@@ -7,24 +7,28 @@ import zio.schema.codec.{BinaryCodec, ProtobufCodec}
 object Main extends ZIOAppDefault {
   private val greetRoute =
     Method.GET / "greet" -> handler { (req: Request) =>
-      val name = req.queryOrElse[String]("name", "World")
-      val key = s"greet:$name"
-      (for {
-        redis <- ZIO.service[Redis]
-        cached <- redis.get(key).returning[String]
-        result <- cached match {
-          case Some(g) => ZIO.succeed((g, true))
-          case None =>
-            val g = s"Hello $name!"
-            redis.set(key, g, Some(5.minutes)).as((g, false))
-        }
-        (greeting, wasCached) = result
-        resp = Response.text(greeting).addHeader("X-Cache", if (wasCached) "HIT" else "MISS")
-      } yield resp)
-        .catchAll(e => ZIO.succeed(Response.text(s"Redis error: ${e.getMessage}").status(Status.InternalServerError)))
+      ZIO.logInfo(s"Called /greet with queryParams=${req.url.queryParams("name")}") *> {
+        val name = req.queryOrElse[String]("name", "World")
+        val key = s"greet:$name"
+        (for {
+          redis <- ZIO.service[Redis]
+          cached <- redis.get(key).returning[String]
+          result <- cached match {
+            case Some(g) => ZIO.succeed((g, true))
+            case None =>
+              val g = s"Hello $name!"
+              redis.set(key, g, Some(5.minutes)).as((g, false))
+          }
+          (greeting, wasCached) = result
+          resp = Response.text(greeting).addHeader("X-Cache", if (wasCached) "HIT" else "MISS")
+        } yield resp)
+          .catchAll(e => ZIO.succeed(Response.text(s"Redis error: ${e.getMessage}").status(Status.InternalServerError)))
+      }
     }
 
-  private val healthRoute = Method.GET / Root -> handler(Response.text("Greetings at your service"))
+  private val healthRoute = Method.GET / "health-check" -> handler {
+    ZIO.logInfo("Called /health-check") *> ZIO.succeed(Response.text("Ok"))
+  }
 
   private val routes: Routes[Redis, Nothing] = Routes(
     healthRoute,
